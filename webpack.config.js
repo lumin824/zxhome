d9d3e77556a8b8148b9c2ccc2ebc32a7cbe907f6
lumin824 <lumin824@163.com>
2017-03-17 23:20:31 +0800

    1

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry:'./src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist2')
+  },
+  module: {
+    loaders: [
+      {test: /\.png$/, loader: 'url-loader?mimetype=image/png&limit=1000'},
+      {test: /\.js$/, use: ['file-loader']},
+      {test: /\.css$/, use: ['file-loader']},
+      {test: /\.html$/, use: [
+        {loader: 'html-loader', options:{
+          //minimize:true,
+          removeComments:true,
+          attrs:['img:src', 'a:href', 'link:href', 'script:src']
+        }}
+      ]},
+
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template:'./src/index.html',
+    })
+  ]
+}
