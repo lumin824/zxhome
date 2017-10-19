9f24d042ecdbd0ecb8c10c2fedb8834dc98c4f98
lumin824 <lumin824@163.com>
2017-10-19 09:16:15 +0800

    111

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,48 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  devtool: 'source-map',
   entry:'./src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
+  plugins: [
+    new ExtractTextPlugin({
+      filename:'[name].[chunkhash].css',
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template:'./src/index.html',
+    }),
+
+    // new HtmlWebpackPlugin({
+    //   filename: 'index_en.html',
+    //   template:'./src/index_en.html',
+    // }),
+    //extractCSS,
+  ],
   module: {
     loaders: [
-      {test: /\.png$/, loader: 'url-loader?mimetype=image/png&limit=1000'},
+      {test: /\.png$/, use: 'url-loader?mimetype=image/png&limit=1000'},
       {test: /\.js$/, use: ['file-loader']},
-      {test: /\.css$/, use: ['file-loader']},
+      {test: /\.(gif|ttf|svg|woff|woff2|eot)$/, loader: 'file-loader'},
+      //{test: /\.css$/, use: },
+      {test:/\.css$/, use: ExtractTextPlugin.extract({
+        use:['css-loader'],
+        fallback:'style-loader'
+      })},
       {test: /\.html$/, use: [
         {loader: 'html-loader', options:{
-          //minimize:true,
-          removeComments:true,
           attrs:['img:src', 'a:href', 'link:href', 'script:src','img:data-original']
         }}
       ]},
 
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template:'./src/index.html',
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index_en.html',
-      template:'./src/index_en.html',
-    })
-  ]
+
 }
